53f7452d1e3be99b27778c8a8174d7b232b97da2
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2021-09-25 14:12:59 -0400

    commit tree 07ce79b1286219ca5c716c43358e84a0a0015f7f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`}
+
+// function wrapAdjective(){
+//     let emphatic = result("a hard worker")
+//     let result = wrapAdjective(){
+//     }
+
+    // function genericCompliment (basicCompliment="special"){
+    //     return `You are ${basicCompliment}`}
+    function wrapAdjective(character="*") {
+        return function (string="special") {
+          return `You are ${character}${string}${character}!`
+        }
+    }
+      
+        // wrapAdjective(outer)(inner)(inner of inner)
+
+
+// wrapAdjective("*")("a dedicated programmer");
+// wrapAdjective("|")("a hard worker");
+
+// wrapAdjective("$")("a hard worker") //"You are $a hard worker$!"
+// wrapAdjective()("a hard worker") //"You are *a hard worker*!"
+// wrapAdjective()() //"You are *special*!"
